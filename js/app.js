8fc529a7d66bdb1188080a6920c0706d4fbb12d9
r03eRt <morgadoluengo@gmail.com>
2017-03-06 15:11:24 +0100

    Added keywords and titles and description, base url and fixes

--- js/app.js
+++ js/app.js
@@ -1,24 +1,45 @@
-var app = angular.module('kubide', ['ui.router']);
+var app = angular.module('kubide', ['ui.router', 'youtube-embed']);
+
+
+app.config(["$locationProvider", function($locationProvider) {
+  $locationProvider.html5Mode(true);
+}]);
 
 app.config(function($stateProvider) {
   var homeState = {
     name: 'home',
     url: '/',
-    //template: '<h3>Home</h3>'
-    templateUrl: '../templates/home.html'
+    templateUrl: '../templates/home.html',
+    controller: 'homeController',
+    controllerAs: 'homeCtrl',
+    data : { 
+      pageTitle: 'Angular Rocks',
+      pageDescription: 'Esta es la descripcion por defecto',
+      pageKeyWords: 'angular, rocks, default'   
+    }
   }
 
   var fooState = {
     name: 'foo',
     url: '/foo',
     //template: '<h3>Foo</h3>'
-    templateUrl: '../templates/foo.html'
+    templateUrl: '../templates/foo.html',
+    data : { 
+      pageTitle: 'Angular Rocks',
+      pageDescription: 'Esta es la descripcion por defecto',
+      pageKeyWords: 'angular, rocks, default'   
+    }
   }
 
   var barState = {
     name: 'bar',
     url: '/bar',
-    templateUrl: '../templates/bar.html'
+    templateUrl: '../templates/bar.html',
+    data : { 
+      pageTitle: 'Angular Rocks',
+      pageDescription: 'Esta es la descripcion por defecto',
+      pageKeyWords: 'angular, rocks, default'   
+    }
   }
 
   var rollingStonesState = {
@@ -27,25 +48,38 @@ app.config(function($stateProvider) {
     //template: '<h3>The rolling stones</h3>'
     templateUrl: '../templates/theRollingStones.html',
     controller: 'theRollingStonesController',
-    controllerAs: 'theRollingStonesCtrl'
+    controllerAs: 'theRollingStonesCtrl',
+    data : { 
+      pageTitle: 'The Rolling Stones',
+      pageDescription: 'The Rolling Stones es una banda británica de rock originaria de Londres. La banda se fundó en abril de 19622 por Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Ian Stewart y Charlie Watts.2 Brian Jones fue despedido en junio de 1969, falleciendo tres semanas después, siendo reemplazado por el guitarrista Mick Taylor, que dejaría el grupo en 1975 y sería a su vez reemplazado por Ron Wood. Con el retiro de Bill Wyman en 1993 se incluyó al bajista Darryl Jones que, aunque toca con la banda desde la grabación del álbum Voodoo Lounge en 1994, no es un miembro oficial.',
+      pageKeyWords: 'rolling, rock, stones'   
+    }
   }
 
   var theBeatlesState = {
     name: 'thebeatles',
     url: '/the-beatles',
-    //template: '<h3>The Beatles</h3>'
     templateUrl: '../templates/theBeatles.html',
     controller: 'theBeatlesController',
-    controllerAs: 'theBeatlesCtrl'
+    controllerAs: 'theBeatlesCtrl',
+    data : { 
+      pageTitle: 'The Beatles',
+      pageDescription: 'The Beatles fue una banda de pop/rock inglesa activa durante la década de 1960, y reconocida como la más exitosa comercialmente y críticamente aclamada en la historia de la música popular.1 2 3 4 5 Formada en Liverpool, estuvo constituida desde 1962 por John Lennon (guitarra rítmica, vocalista), Paul McCartney (bajo, vocalista), George Harrison (guitarra solista, vocalista) y Ringo Starr (batería, vocalista). ',
+      pageKeyWords: 'pop, rock, Londres, 1960'   
+    }
   }
 
   var queenState = {
     name: 'queen',
     url: '/queen',
-    //template: '<h3>Queen</h3>'
     templateUrl: '../templates/queen.html',
     controller: 'queenController',
-    controllerAs: 'queenCtrl'
+    controllerAs: 'queenCtrl',
+    data : { 
+      pageTitle: 'Queen',
+      pageDescription: 'Queen es una banda británica de rock formada en 1970 en Londres por el cantante Freddie Mercury, el guitarrista Brian May, el baterista Roger Taylor y el bajista John Deacon. Si bien el grupo ha presentado bajas de dos de sus miembros (Mercury, fallecido en 1991, y Deacon, retirado en 1997), los integrantes restantes, May y Taylor, continúan trabajando bajo el nombre Queen, por lo que la banda aún es considerada activa.',
+      pageKeyWords: 'Queen, rock, banda, Londres'   
+    }
   }
 
   $stateProvider.state(homeState);
@@ -55,3 +89,10 @@ app.config(function($stateProvider) {
   $stateProvider.state(theBeatlesState);
   $stateProvider.state(queenState);
 });
+
+
+app.run([ '$rootScope', '$state', '$stateParams',
+function ($rootScope, $state, $stateParams) {
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+}])
--- js/controllers/theRollingStonesController.js
+++ js/controllers/theRollingStonesController.js
@@ -7,8 +7,8 @@ app.controller('theRollingStonesController', ['$scope', function($scope)
 	//bind data
 	this.group = {
 		title: 'The rolling stones data',
-		youtube: ''
+		youtube: 'vC0Qt1lvLq8',
+		text: 'The Rolling Stones es una banda británica de rock originaria de Londres. La banda se fundó en abril de 19622 por Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Ian Stewart y Charlie Watts.2 Brian Jones fue despedido en junio de 1969, falleciendo tres semanas después, siendo reemplazado por el guitarrista Mick Taylor, que dejaría el grupo en 1975 y sería a su vez reemplazado por Ron Wood. Con el retiro de Bill Wyman en 1993 se incluyó al bajista Darryl Jones que, aunque toca con la banda desde la grabación del álbum Voodoo Lounge en 1994, no es un miembro oficial.'
 	}
-	this.title = 'the rolling stones data';
 
 }]);
